59b099167f27d77a6cbf5986e062ca2a9315ce48
510208 <xux510208@gmail.com>
2024-10-27 17:19:32 +0800

    Update .gitignore to ignore build and dist directories and add TOKEN

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ URL = "https://github.com/510208/mineplayer"
 DOWNLOAD_URL = "https://pypi.org/project/mineplayer/"
 
 LICENSE = "GNU General Public License v3.0"
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 DESCRIPTION = "A module to parse Minecraft player information"
 LONG_DESCRIPTION = (HERE / "docs" / "README.md").read_text(encoding="utf8")
 LONG_DESC_TYPE = "text/markdown"
