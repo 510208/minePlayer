fa180de911d98bece9274e41d3f2d9ad81ec1389
SamHacker <xux510208@gmail.com>
2024-10-31 13:39:10 +0800

    Update setup.py to 1.0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ URL = "https://github.com/510208/minePlayer"
 DOWNLOAD_URL = "https://pypi.org/project/mineplayer/"
 
 LICENSE = "GNU General Public License v3.0"
-VERSION = "1.0.3"
+VERSION = "1.0.4"
 DESCRIPTION = "A module to parse Minecraft player information"
 LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding="utf8")
 LONG_DESC_TYPE = "text/markdown"
